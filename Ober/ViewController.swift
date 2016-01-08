c482024665c3d156b2a02c1facc55d18591e4d92
Mike Rizza <mikerizza@mikes-macbook-pro.local>
2016-01-07 18:13:17 -0600

    intro video does not repeat and when video is over label pops up

--- Ober/ViewController.swift
+++ Ober/ViewController.swift
@@ -31,23 +31,66 @@ class ViewController: UIViewController {
     }
 
 
-//setup to play video file
-func setupView() {
+    //setup to play video file
+    func setupView() {
     
-    let path = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("introVideo", ofType: "mov")!)
+        let path = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("introVideo", ofType: "mov")!)
     
-    let player = AVPlayer(URL: path)
+        let player = AVPlayer(URL: path)
     
-    let newLayer = AVPlayerLayer(player: player)
-    newLayer.frame = self.videoView.frame
-    self.videoView.layer.addSublayer(newLayer)
-    newLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        let newLayer = AVPlayerLayer(player: player)
+        newLayer.frame = self.videoView.frame
+        self.videoView.layer.addSublayer(newLayer)
+        newLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        //set text to play over video view
+      //  self.oberTextCenter(self.videoView) // view we put text on
+        
+        player.play()
+        
+        // set it to do nothing when video ends
+        player.actionAtItemEnd = AVPlayerActionAtItemEnd.None
     
-    player.play()
-    
+        //set notification when video ends
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "videoDidPlayToEnd:", name: "AVPlayerItemDidPlayToEndTimeNotification", object: player.currentItem)
     
 }
-
+    //what to do when video ends
+    func videoDidPlayToEnd(notification: NSNotification){
+        
+        
+        //replay video when it ends
+        /*let player: AVPlayerItem = notification.object as! AVPlayerItem
+          player.seekToTime(kCMTimeZero)*/
+        
+       
+        
+        //create label
+        self.oberTextCenter(self.videoView)
+        
+        
+    }
+    
+    //create and set label
+    func oberTextCenter(containerView: UIView!){
+        
+        let half:CGFloat = 1.0 / 2.0
+        
+        //label
+        let oberLabel = UILabel()
+        
+        //label attributes
+        oberLabel.text = "Da Bulls"
+        oberLabel.font = UIFont(name: "Apple Color Emoji", size:  120.0)
+        oberLabel.backgroundColor = UIColor.clearColor()
+        oberLabel.textColor = UIColor.redColor() //color of text
+        oberLabel.sizeToFit()
+        oberLabel.textAlignment = NSTextAlignment.Center
+        oberLabel.frame.origin.x = (containerView.frame.size.width - oberLabel.frame.size.width) * half
+        oberLabel.frame.origin.y = (containerView.frame.size.height - oberLabel.frame.size.height) * half
+        containerView.addSubview(oberLabel)
+        
+    }
 
 }
 
