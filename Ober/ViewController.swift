6a2a7e578dc2da646fd6af971fce654747cdab04
Mike Rizza <mikerizza@mikes-macbook-pro.local>
2016-01-07 19:22:16 -0600

    add segue to signup view controller and added username and password text fields

--- Ober/ViewController.swift
+++ Ober/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
         newLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
         
         //set text to play over video view
-      //  self.oberTextCenter(self.videoView) // view we put text on
+        self.oberTextCenter(self.videoView) // view we put text on
         
         player.play()
         
@@ -53,6 +53,9 @@ class ViewController: UIViewController {
     
         //set notification when video ends
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "videoDidPlayToEnd:", name: "AVPlayerItemDidPlayToEndTimeNotification", object: player.currentItem)
+        
+        //add buttons to video view
+        self.createOberButtons(self.videoView)
     
 }
     //what to do when video ends
@@ -60,13 +63,13 @@ class ViewController: UIViewController {
         
         
         //replay video when it ends
-        /*let player: AVPlayerItem = notification.object as! AVPlayerItem
-          player.seekToTime(kCMTimeZero)*/
+        let player: AVPlayerItem = notification.object as! AVPlayerItem
+          player.seekToTime(kCMTimeZero)
         
        
         
         //create label
-        self.oberTextCenter(self.videoView)
+        //self.oberTextCenter(self.videoView)
         
         
     }
@@ -79,7 +82,7 @@ class ViewController: UIViewController {
         //label
         let oberLabel = UILabel()
         
-        //label attributes
+        //label properties
         oberLabel.text = "Da Bulls"
         oberLabel.font = UIFont(name: "Apple Color Emoji", size:  120.0)
         oberLabel.backgroundColor = UIColor.clearColor()
@@ -92,6 +95,64 @@ class ViewController: UIViewController {
         
     }
 
+    //create signin and register buttons
+    func createOberButtons(containerView: UIView){
+        
+        let margin:CGFloat = 15.0
+        let middleSpacing:CGFloat = 7.5
+        
+        let signIn = UIButton()  //sign in button
+        
+        //properties of sign in
+        signIn.setTitle("signIn", forState: .Normal)
+        signIn.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        signIn.backgroundColor = UIColor.greenColor()
+        signIn.frame.size.width = (((containerView.frame.size.width - signIn.frame.size.width) - (margin * 2)) / 2 - middleSpacing)
+        signIn.frame.size.height = 40.0
+        signIn.frame.origin.x = margin
+        signIn.frame.origin.y = ((containerView.frame.size.height - signIn.frame.size.height) - 25)
+        
+        //add target
+        signIn.addTarget(self, action: "signInButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        containerView.addSubview(signIn)
+        
+        
+        let register = UIButton()//sign in button
+        
+        //properties of sign in
+        register.setTitle("Register", forState: .Normal)
+        register.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        register.backgroundColor = UIColor.greenColor()
+        register.frame.size.width = (((containerView.frame.size.width - register.frame.size.width) - (margin * 2)) / 2 - middleSpacing)
+        register.frame.size.height = 40.0
+        register.frame.origin.x = ((containerView.frame.size.width - register.frame.size.width) - margin)
+        register.frame.origin.y = ((containerView.frame.size.height - register.frame.size.height) - 25)
+        
+        //add target
+        register.addTarget(self, action: "registerButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        containerView.addSubview(register)
+        
+        
+    }
+    
+    
+    //navigate to signUp view controller
+    func signInButtonPressed(sender: UIButton!){
+        
+        //access mainStoryBoard
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let signUpVC: SignUpViewController = storyBoard.instantiateViewControllerWithIdentifier("signUp") as! SignUpViewController
+        
+        let navigationController = UINavigationController(rootViewController: signUpVC)
+        
+        self.presentViewController(navigationController, animated: true, completion: nil)
+    }
+    
+    func registerButtonPressed(sender: UIButton!){
+        
+        print("Register")
+    }
+    
 }
 
 
